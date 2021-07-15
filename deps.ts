60a5f99a4ef1a5efa713142f6a641ddef0d60643
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-07-15 20:56:13 +0800

    Deps: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import { delay } from 'https://deno.land/std@0.97.0/async/delay.ts'
+import { delay } from 'https://deno.land/std@0.101.0/async/delay.ts'
 
 export const Std = { delay };
-export * as Discord from 'https://deno.land/x/discordeno@11.0.2/mod.ts';
+export * as Discord from 'https://deno.land/x/discordeno@12.0.1/mod.ts';
 export * as Dotenv from 'https://deno.land/x/dotenv@v2.0.0/mod.ts';
-export * as Zod from 'https://deno.land/x/zod@v3.1.0/external.ts';
+export * as Zod from 'https://deno.land/x/zod@v3.5.1/external.ts';
