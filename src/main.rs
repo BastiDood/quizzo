aae3e323fbbd40369a6290c704361318ea1df97d
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-09-11 00:14:03 +0800

    Feat: prepare scaffolding for Hyper server framework

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Response, {Error as HyperError, Server},
+};
 use std::{
-    env::{var, VarError},
+    convert::Infallible,
+    env::{self, VarError},
+    future,
     io::Error as IoError,
+    net::{Ipv4Addr, TcpListener},
     num::NonZeroU64,
 };
 use tokio::runtime::Builder;
 
 #[derive(Debug)]
 enum AppError {
+    ServerInit,
     MissingEnvVars,
+    Hyper(HyperError),
     Io(IoError),
 }
 
@@ -23,18 +32,41 @@ impl From<IoError> for AppError {
     }
 }
 
+impl From<HyperError> for AppError {
+    fn from(err: HyperError) -> Self {
+        Self::Hyper(err)
+    }
+}
+
 fn main() -> Result<(), AppError> {
     // Retrieve environment variables
-    let bot_token = var("BOT_TOKEN")?;
-    let application_id = var("APPLICATION_ID")?
+    let port = env::var("PORT")?
+        .parse()
+        .map_err(|_| AppError::MissingEnvVars)?;
+    let bot_token = env::var("BOT_TOKEN")?;
+    let application_id = env::var("APPLICATION_ID")?
         .parse::<u64>()
         .map_err(|_| AppError::MissingEnvVars)?;
-    let guild_id = var("GUILD_ID")?
+    let guild_id = env::var("GUILD_ID")?
         .parse::<u64>()
         .ok()
         .and_then(NonZeroU64::new);
 
+    // Configure main service
+    let service = make_service_fn(|_| {
+        let outer =
+            service_fn(|_| future::ready(Ok::<_, Infallible>(Response::new(Body::empty()))));
+        future::ready(Ok::<_, Infallible>(outer))
+    });
+
+    // Configure server
+    let tcp = TcpListener::bind((Ipv4Addr::UNSPECIFIED, port))?;
+    let server = Server::from_tcp(tcp)?.http1_only(true).serve(service);
+
     // Launch Tokio async runtime
-    let runtime = Builder::new_current_thread().enable_all().build()?;
+    Builder::new_current_thread()
+        .enable_io()
+        .build()?
+        .block_on(server)?;
     Ok(())
 }
