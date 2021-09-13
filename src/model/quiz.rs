98c8bb866bc3274985a19b651e0f68c1a5545eae
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-09-13 22:16:56 +0800

    Feat: deserialize the timeout as duration from `u64`

--- src/model/quiz.rs
+++ src/model/quiz.rs
@@ -1,9 +1,19 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use std::time::Duration;
+
+fn deserialize_as_duration<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let secs = u64::deserialize(deserializer)?;
+    Ok(Duration::from_secs(secs))
+}
 
 #[derive(Deserialize)]
 pub struct Quiz<'a> {
     pub question: &'a str,
     pub answer: usize,
     pub choices: Box<[&'a str]>,
-    pub timeout: u64,
+    #[serde(deserialize_with = "deserialize_as_duration")]
+    pub timeout: Duration,
 }
