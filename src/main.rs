881f70b8f1396d0bb195a0b2e8b0a25a49a8405e
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-14 02:42:20 +0800

    Feat: implement main service for server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use hyper::{service, Server};
+use hyper::{
+    body::{self, Buf},
+    service, Body, Response, Server, Uri,
+};
 use quizzo::Lobby;
 use std::{
-    env,
+    convert::Infallible,
+    env, future,
     net::{Ipv4Addr, SocketAddr},
 };
 use tokio::runtime::Runtime;
@@ -16,15 +20,34 @@ fn main() -> anyhow::Result<()> {
         _ => None,
     };
 
-    // Create service handler
-    let service = service::make_service_fn(move |_| async move {
-        let lobby = Lobby::new(token, app, maybe_guild_id).await?;
-        todo!()
+    // Initialize service handler
+    let runtime = Runtime::new()?;
+    let future = Lobby::new(token, app, maybe_guild_id);
+    let lobby = runtime.block_on(future)?;
+    let service = service::make_service_fn(move |_| {
+        let lobby_outer = lobby.clone();
+        future::ready(Ok::<_, Infallible>(service::service_fn(move |req| {
+            let lobby_inner = lobby_outer.clone();
+            async move {
+                // For now, we only allow requests from the root endpoint.
+                anyhow::ensure!(req.uri() == &Uri::from_static("/"));
+
+                // Parse incoming interaction
+                let body = req.into_body();
+                let reader = body::aggregate(body).await?.reader();
+                let interaction = serde_json::from_reader(reader)?;
+
+                // Reply to the server
+                let response = lobby_inner.on_interaction(interaction).await;
+                let body: Body = serde_json::to_vec(&response)?.into();
+                anyhow::Ok(Response::new(body))
+            }
+        })))
     });
 
     // Run the server
     let addr: SocketAddr = (Ipv4Addr::UNSPECIFIED, port).into();
-    let server = Server::bind(&addr).serve(service);
-    let res = Runtime::new()?.block_on(server)?;
-    anyhow::Ok(res)
+    let server = Server::bind(&addr);
+    runtime.block_on(server.serve(service))?;
+    Ok(())
 }
