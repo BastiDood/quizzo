d0e3cdd44f639387b4d73e5123db3bf792f5e98e
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-13 18:03:34 +0800

    Chore: prepare scaffolding for lobby handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,50 @@
 use dashmap::DashMap;
+use hyper::{client::HttpConnector, Client};
+use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
 use tokio::sync::mpsc;
 
 use twilight_http::client::InteractionClient;
-use twilight_model::id::{marker::InteractionMarker, Id};
+use twilight_model::{
+    application::interaction::{ApplicationCommand, MessageComponentInteraction},
+    id::{marker::InteractionMarker, Id},
+};
 
 type Key = Id<InteractionMarker>;
 type Channel = mpsc::Sender<()>;
 
 pub struct Lobby<'client> {
+    /// Container for all pending polls.
     polls: DashMap<Key, Channel>,
-    client: InteractionClient<'client>,
+    /// Discord API interactions.
+    api: InteractionClient<'client>,
+    /// Arbitrary HTTP fetching of JSON files.
+    http: Client<HttpsConnector<HttpConnector>>,
 }
 
 impl<'c> From<InteractionClient<'c>> for Lobby<'c> {
-    fn from(client: InteractionClient<'c>) -> Self {
+    fn from(api: InteractionClient<'c>) -> Self {
+        let connector = HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_only()
+            .enable_http2()
+            .build();
+        let http = Client::builder().http2_only(true).build(connector);
         Self {
-            client,
+            api,
+            http,
             polls: DashMap::new(),
         }
+    }
+}
+
+impl Lobby<'_> {
+    /// Responds to new application commands.
+    pub async fn on_app_comm(&self, comm: ApplicationCommand) {
+        todo!()
+    }
+
+    /// Responds to message component interactions.
+    pub async fn on_msg_interaction(&self, comm: MessageComponentInteraction) {
+        todo!()
     }
 }
