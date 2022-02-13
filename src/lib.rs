53c269eecb74ad7e2b0790e570fce25050ee84c9
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-13 19:17:56 +0800

    Feat: implement register for command ID

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,18 @@ mod quiz;
 use dashmap::DashMap;
 use hyper::Client;
 use hyper_trust_dns::RustlsHttpsConnector;
-use quiz::Quiz;
 use tokio::sync::mpsc;
 
 use twilight_http::client::InteractionClient;
 use twilight_model::{
-    application::interaction::{ApplicationCommand, MessageComponentInteraction},
-    id::{marker::InteractionMarker, Id},
+    application::{
+        command::{ChoiceCommandOptionData, CommandOption},
+        interaction::{ApplicationCommand, MessageComponentInteraction},
+    },
+    id::{
+        marker::{CommandMarker, GuildMarker, InteractionMarker},
+        Id,
+    },
 };
 
 type Key = Id<InteractionMarker>;
@@ -22,21 +27,64 @@ pub struct Lobby<'client> {
     api: InteractionClient<'client>,
     /// Arbitrary HTTP fetching of JSON files.
     http: Client<RustlsHttpsConnector>,
+    /// Command ID to match on.
+    command: Id<CommandMarker>,
 }
 
-impl<'c> From<InteractionClient<'c>> for Lobby<'c> {
-    fn from(api: InteractionClient<'c>) -> Self {
+impl<'c> Lobby<'c> {
+    pub fn new(api: InteractionClient<'c>, command: Id<CommandMarker>) -> Self {
         let connector = hyper_trust_dns::new_rustls_native_https_connector();
         let http = Client::builder().http2_only(true).build(connector);
         Self {
             api,
             http,
+            command,
             quizzes: DashMap::new(),
         }
     }
 }
 
 impl Lobby<'_> {
+    /// Registers the quiz creation command.
+    pub async fn register<'c>(
+        api: InteractionClient<'c>,
+        maybe_guild_id: Option<Id<GuildMarker>>,
+    ) -> anyhow::Result<Id<CommandMarker>> {
+        const CREATE_NAME: &str = "create";
+        const CREATE_DESC: &str = "Create a quiz from JSON data.";
+
+        let options = [CommandOption::String(ChoiceCommandOptionData {
+            autocomplete: false,
+            choices: Vec::new(),
+            description: String::from("URL from which to fetch JSON data."),
+            required: true,
+            name: String::from("url"),
+        })];
+
+        let command_fut = if let Some(guild_id) = maybe_guild_id {
+            api.create_guild_command(guild_id)
+                .chat_input(CREATE_NAME, CREATE_DESC)
+                .unwrap()
+                .command_options(&options)
+                .unwrap()
+                .exec()
+        } else {
+            api.create_global_command()
+                .chat_input(CREATE_NAME, CREATE_DESC)
+                .unwrap()
+                .command_options(&options)
+                .unwrap()
+                .exec()
+        };
+
+        command_fut
+            .await?
+            .model()
+            .await?
+            .id
+            .ok_or_else(|| anyhow::Error::msg("absent command ID"))
+    }
+
     /// Responds to new application commands.
     pub async fn on_app_comm(&self, comm: ApplicationCommand) {
         todo!()
