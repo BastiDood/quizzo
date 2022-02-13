cc1d168c76fdf1d7d7c642c2e83732ab70e0beb4
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-13 19:48:32 +0800

    Feat: add validation steps for command handler

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,11 @@ use tokio::sync::mpsc;
 use twilight_http::client::InteractionClient;
 use twilight_model::{
     application::{
+        callback::{CallbackData, InteractionResponse},
         command::{ChoiceCommandOptionData, CommandOption},
-        interaction::{ApplicationCommand, MessageComponentInteraction},
+        interaction::{ApplicationCommand, Interaction, MessageComponentInteraction},
     },
+    channel::message::MessageFlags,
     id::{
         marker::{CommandMarker, GuildMarker, InteractionMarker},
         Id,
@@ -45,14 +47,14 @@ impl<'c> Lobby<'c> {
 }
 
 impl Lobby<'_> {
+    const CREATE_NAME: &'static str = "create";
+    const CREATE_DESC: &'static str = "Create a quiz from JSON data.";
+
     /// Registers the quiz creation command.
     pub async fn register<'c>(
         api: InteractionClient<'c>,
         maybe_guild_id: Option<Id<GuildMarker>>,
     ) -> anyhow::Result<Id<CommandMarker>> {
-        const CREATE_NAME: &str = "create";
-        const CREATE_DESC: &str = "Create a quiz from JSON data.";
-
         let options = [CommandOption::String(ChoiceCommandOptionData {
             autocomplete: false,
             choices: Vec::new(),
@@ -63,14 +65,14 @@ impl Lobby<'_> {
 
         let command_fut = if let Some(guild_id) = maybe_guild_id {
             api.create_guild_command(guild_id)
-                .chat_input(CREATE_NAME, CREATE_DESC)
+                .chat_input(Self::CREATE_NAME, Self::CREATE_DESC)
                 .unwrap()
                 .command_options(&options)
                 .unwrap()
                 .exec()
         } else {
             api.create_global_command()
-                .chat_input(CREATE_NAME, CREATE_DESC)
+                .chat_input(Self::CREATE_NAME, Self::CREATE_DESC)
                 .unwrap()
                 .command_options(&options)
                 .unwrap()
@@ -85,13 +87,52 @@ impl Lobby<'_> {
             .ok_or_else(|| anyhow::Error::msg("absent command ID"))
     }
 
+    pub async fn on_interaction(&self, interaction: Interaction) -> InteractionResponse {
+        use Interaction::*;
+        match interaction {
+            Ping(_) => todo!(),
+            ApplicationCommand(comm) => self.on_app_comm(*comm).await,
+            MessageComponent(msg) => self.on_msg_interaction(*msg).await,
+            _ => InteractionResponse::ChannelMessageWithSource(CallbackData {
+                content: Some(String::from("Unsupported interaction.")),
+                flags: Some(MessageFlags::EPHEMERAL),
+                tts: None,
+                embeds: None,
+                components: None,
+                allowed_mentions: None,
+            }),
+        }
+    }
+
     /// Responds to new application commands.
-    pub async fn on_app_comm(&self, comm: ApplicationCommand) {
+    pub async fn on_app_comm(&self, comm: ApplicationCommand) -> InteractionResponse {
+        if comm.data.id != self.command {
+            return InteractionResponse::ChannelMessageWithSource(CallbackData {
+                content: Some(String::from("Unknown command ID.")),
+                flags: Some(MessageFlags::EPHEMERAL),
+                tts: None,
+                embeds: None,
+                components: None,
+                allowed_mentions: None,
+            });
+        }
+
+        if comm.data.name.as_str() != Self::CREATE_NAME {
+            return InteractionResponse::ChannelMessageWithSource(CallbackData {
+                content: Some(String::from("Unknown command name.")),
+                flags: Some(MessageFlags::EPHEMERAL),
+                tts: None,
+                embeds: None,
+                components: None,
+                allowed_mentions: None,
+            });
+        }
+
         todo!()
     }
 
     /// Responds to message component interactions.
-    pub async fn on_msg_interaction(&self, comm: MessageComponentInteraction) {
+    pub async fn on_msg_interaction(&self, msg: MessageComponentInteraction) -> InteractionResponse {
         todo!()
     }
 }
