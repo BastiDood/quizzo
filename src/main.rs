5c398dda14fcfae079d59a90a1962bccc4449d84
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-15 00:48:02 +0800

    Fix: move service initialization inside runtime

--- src/main.rs
+++ src/main.rs
@@ -23,37 +23,37 @@ fn main() -> anyhow::Result<()> {
     let app = env::var("APP_ID")?.parse()?;
     let token = env::var("TOKEN")?;
 
-    // Prepare server
-    let lobby = Lobby::new(token, app);
-    let service = hyper::service::make_service_fn(move |_| {
-        let lobby_outer = lobby.clone();
-        let public_outer = public.clone();
-        future::ready(Ok::<_, Infallible>(hyper::service::service_fn(move |req| {
-            let lobby_inner = lobby_outer.clone();
-            let public_inner = public_outer.clone();
-            async move {
-                let future = service::try_respond(req, &lobby_inner, &public_inner);
-                let response = match future.await {
-                    Ok(bytes) => {
-                        let mut response = Response::new(Body::from(bytes));
-                        response
-                            .headers_mut()
-                            .append(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-                        response
-                    }
-                    Err(code) => {
-                        let mut response = Response::new(Body::empty());
-                        *response.status_mut() = code;
-                        response
-                    }
-                };
-                Ok::<_, Infallible>(response)
-            }
-        })))
-    });
-
     // Run server
+    let lobby = Lobby::new(token, app);
     let addr: SocketAddr = (Ipv4Addr::UNSPECIFIED, port).into();
-    Runtime::new()?.block_on(Server::bind(&addr).serve(service))?;
+    Runtime::new()?.block_on(async move {
+        let service = hyper::service::make_service_fn(move |_| {
+            let lobby_outer = lobby.clone();
+            let public_outer = public.clone();
+            future::ready(Ok::<_, Infallible>(hyper::service::service_fn(move |req| {
+                let lobby_inner = lobby_outer.clone();
+                let public_inner = public_outer.clone();
+                async move {
+                    let future = service::try_respond(req, &lobby_inner, &public_inner);
+                    let response = match future.await {
+                        Ok(bytes) => {
+                            let mut response = Response::new(Body::from(bytes));
+                            response
+                                .headers_mut()
+                                .append(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+                            response
+                        }
+                        Err(code) => {
+                            let mut response = Response::new(Body::empty());
+                            *response.status_mut() = code;
+                            response
+                        }
+                    };
+                    Ok::<_, Infallible>(response)
+                }
+            })))
+        });
+        Server::bind(&addr).serve(service).await
+    })?;
     Ok(())
 }
