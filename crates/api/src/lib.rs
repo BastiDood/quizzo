19b3ccc906a6bc3cc6b626f0d6af2247498e15f6
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-04-02 22:12:17 +0800

    Feat(api): implement server-side quiz validation

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -4,6 +4,13 @@ use mongodb::{results::InsertOneResult, Collection};
 
 /// Attempts to create a new quiz. Returns the ObjectID of the document.
 pub async fn try_submit_quiz(col: &Collection<Quiz>, quiz: &Quiz) -> Result<[u8; 12], StatusCode> {
+    // Validate the quiz
+    let choice_count = quiz.choices.len();
+    if usize::from(quiz.answer) >= choice_count || !(1..=25).contains(&choice_count) {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    // Attempt to submit to MongoDB
     let InsertOneResult { inserted_id, .. } = col.insert_one(quiz, None).await.map_err(|err| {
         use mongodb::error::{ErrorKind, WriteError, WriteFailure};
         match *err.kind {
@@ -11,6 +18,8 @@ pub async fn try_submit_quiz(col: &Collection<Quiz>, quiz: &Quiz) -> Result<[u8;
             _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
     })?;
+
+    // Attempt to parse as ObjectID
     let oid = inserted_id.as_object_id().ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
     Ok(oid.bytes())
 }
