c7e71a2fd98043b755bf9233ecc1520d403ec081
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-06-07 22:09:56 +0800

    Feat: setup infrastructure for database wrapper

--- crates/api/src/auth.rs
+++ crates/api/src/auth.rs
@@ -1,50 +1,13 @@
-use alloc::{boxed::Box, string::String, vec::Vec};
-use core::num::NonZeroU64;
 use hyper::{header::HeaderValue, Response, StatusCode, Uri};
-use mongodb::{
-    bson::{oid::ObjectId, Binary},
-    results::InsertOneResult,
-    Collection,
-};
-use rand_core::{CryptoRng, RngCore};
 
-pub async fn create_session<Rand>(
-    rng: &mut Rand,
-    col: &Collection<Result<NonZeroU64, Binary>>,
-) -> mongodb::error::Result<(ObjectId, Box<str>)>
-where
-    Rand: RngCore + CryptoRng,
-{
-    use mongodb::bson::spec::BinarySubtype;
-    let mut nonce = Vec::from([0; 16]);
-    rng.fill_bytes(nonce.as_mut_slice());
-    let bin = Binary {
-        bytes: nonce,
-        subtype: BinarySubtype::Generic,
-    };
-
-    let mut txt = Vec::from([0; 32]);
-    hex::encode_to_slice(&bin.bytes, &mut txt).unwrap();
-    let encoded = String::from_utf8(txt).unwrap().into_boxed_str();
-
-    let InsertOneResult { inserted_id, .. } = col.insert_one(&Err(bin), None).await?;
-    let oid = inserted_id.as_object_id().unwrap();
-    Ok((oid, encoded))
-}
-
-pub fn create_redirect_response(client_id: &str, redirect_uri: &Uri) -> impl Fn(&str, &str) -> Response<()> {
+pub fn create_redirect_response(client_id: &str, redirect_uri: &Uri) -> impl Fn(&str) -> Response<()> {
     let form = alloc::format!(
-        "https://discord.com/api/oauth2/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&state="
+        "https://discord.com/api/oauth2/authorize?response_type=code&scope=identify&client_id={client_id}&redirect_uri={redirect_uri}&state="
     );
-    move |session, nonce| {
-        let uri = form.clone() + nonce;
-        let cookie = alloc::format!("sid={session}; HttpOnly; SameSite=Lax; Secure");
-
+    move |session| {
+        let uri = form.clone() + session;
         let (mut parts, body) = Response::new(()).into_parts();
         parts.status = StatusCode::FOUND;
-        parts
-            .headers
-            .insert("Set-Cookie", HeaderValue::from_str(&cookie).unwrap());
         parts.headers.insert("Location", HeaderValue::from_str(&uri).unwrap());
         Response::from_parts(parts, body)
     }
---
+++ crates/db/src/lib.rs
@@ -0,0 +1,46 @@
+#![no_std]
+
+use core::num::NonZeroU64;
+use model::Quiz;
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    results::InsertOneResult,
+    Collection, Database,
+};
+
+pub type Session = Option<NonZeroU64>;
+
+pub struct QuizzoDatabase {
+    sessions: Collection<Session>,
+    quizzes: Collection<Quiz>,
+}
+
+impl QuizzoDatabase {
+    pub fn new(db: &Database) -> Self {
+        Self {
+            sessions: db.collection("sessions"),
+            quizzes: db.collection("quizzes"),
+        }
+    }
+
+    pub async fn create_session(&self) -> mongodb::error::Result<ObjectId> {
+        let InsertOneResult { inserted_id, .. } = self.sessions.insert_one(None, None).await?;
+        let id = inserted_id.as_object_id().unwrap();
+        Ok(id)
+    }
+
+    pub async fn upgrade_session(&self, session: ObjectId, user: impl Into<NonZeroU64>) -> mongodb::error::Result<Session> {
+        let old = self
+            .sessions
+            .find_one_and_replace(doc! { "_id": session }, Some(user.into()), None)
+            .await?
+            .unwrap();
+        Ok(old)
+    }
+
+    pub async fn create_quiz(&self, quiz: &Quiz) -> mongodb::error::Result<ObjectId> {
+        let InsertOneResult { inserted_id, .. } = self.quizzes.insert_one(quiz, None).await?;
+        let id = inserted_id.as_object_id().unwrap();
+        Ok(id)
+    }
+}
