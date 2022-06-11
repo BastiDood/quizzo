f470fcf004994672261cb05feed4a5f05f7f6f80
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-06-11 16:48:38 +0800

    Refactor: implement constructor for `App`

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -21,15 +21,23 @@ pub struct App {
     redirector: Redirect,
     /// Exchanges authorization codes for token responses.
     exchanger: CodeExchanger,
+    /// HTTPS/1.0 client for token-related API calls.
+    http: hyper::Client<hyper_trust_dns::RustlsHttpsConnector>,
 }
 
 impl App {
     pub fn new(db: &MongoDb, bot_token: String, client_id: &str, client_secret: &str, redirect_uri: &Uri) -> Self {
+        let connector = hyper_trust_dns::TrustDnsResolver::default().into_rustls_native_https_connector();
+        let http = hyper::Client::builder()
+            .http1_max_buf_size(8192)
+            .set_host(false)
+            .build(connector);
         Self {
             db: Database::new(db),
             discord: twilight_http::Client::new(bot_token),
             exchanger: CodeExchanger::new(client_id, client_secret, redirect_uri),
             redirector: Redirect::new(client_id, redirect_uri),
+            http,
         }
     }
 }
