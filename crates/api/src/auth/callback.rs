e00092657f322cd7cc6f47ca6c081d6eb653fea6
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-06-16 14:37:16 +0800

    Refactor: move `/auth/callback` logic to own module

--- crates/api/src/auth/callback.rs
+++ crates/api/src/auth/callback.rs
@@ -1,5 +1,7 @@
+use crate::HttpClient;
 use alloc::boxed::Box;
-use hyper::{Body, Request};
+use db::Database;
+use hyper::{Body, HeaderMap, Request, Response, StatusCode};
 
 fn parse_code_and_state(query: &str) -> Option<(&str, &str)> {
     let mut code = None;
@@ -46,4 +48,79 @@ impl CodeExchanger {
 
         Some((req, state))
     }
+}
+
+pub async fn try_respond(
+    headers: &HeaderMap,
+    query: &str,
+    exchanger: &CodeExchanger,
+    db: &Database,
+    http: &HttpClient,
+) -> Result<Response<Body>, StatusCode> {
+    use crate::util::session;
+
+    let session = session::extract_session(headers)?;
+    let oid = db::ObjectId::parse_str(session).map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    // Check database if user ID is present
+    let session =
+        db.get_session(oid).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.ok_or(StatusCode::UNAUTHORIZED)?;
+    let nonce = if let model::session::Session::Pending { nonce } = session {
+        nonce
+    } else {
+        return Err(StatusCode::FORBIDDEN);
+    };
+
+    // Hash the salted session ID
+    use ring::digest;
+    let salted = session::salt_session_with_nonce(oid, nonce);
+    let hash = digest::digest(&digest::SHA256, &salted);
+
+    // Parse the `state` parameter as raw bytes
+    let (req, state) = exchanger.generate_token_request(query).ok_or(StatusCode::BAD_REQUEST)?;
+    let mut state_buf = [0; 32];
+    hex::decode_to_slice(state, &mut state_buf).map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    // Validate whether the hash of the session matches
+    if hash.as_ref() != state_buf.as_ref() {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    use hyper::body::{self, Buf};
+    use model::oauth::TokenResponse;
+    let body = http.request(req).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.into_body();
+    let reader = body::aggregate(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.reader();
+    let TokenResponse { access, refresh, expires } =
+        serde_json::from_reader(reader).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    use twilight_model::user::CurrentUser;
+    let client = twilight_http::Client::new(access.clone().into_string());
+    let CurrentUser { id, .. } = client
+        .current_user()
+        .exec()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .model()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let expires = core::time::Duration::from_secs(expires.get());
+    let success = db
+        .upgrade_session(oid, id.into_nonzero(), access, refresh, expires)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    if !success {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    use hyper::header::{HeaderValue, LOCATION};
+    let mut res = Response::new(Body::empty());
+    *res.status_mut() = StatusCode::FOUND;
+
+    if res.headers_mut().insert(LOCATION, HeaderValue::from_static("/")).is_some() {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    Ok(res)
 }
--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -8,7 +8,10 @@ mod quiz;
 mod util;
 
 use alloc::string::String;
-use auth::{login::Redirect, callback::CodeExchanger};
+use auth::{
+    callback::{self, CodeExchanger},
+    login::Redirect,
+};
 use db::Database;
 use hyper::{Body, Request, Response, StatusCode};
 use lobby::Lobby;
@@ -20,6 +23,8 @@ use twilight_model::id::{marker::ApplicationMarker, Id};
 pub use db::{MongoClient, MongoDb, ObjectId};
 pub use hyper::Uri;
 pub type ApplicationId = Id<ApplicationMarker>;
+
+type HttpClient = hyper::Client<hyper_trust_dns::RustlsHttpsConnector>;
 
 pub struct App<Rng, Bytes>
 where
@@ -36,7 +41,7 @@ where
     /// Exchanges authorization codes for token responses.
     exchanger: CodeExchanger,
     /// HTTPS/1.0 client for token-related API calls.
-    http: hyper::Client<hyper_trust_dns::RustlsHttpsConnector>,
+    http: HttpClient,
     /// Public key of the Discord application.
     public: UnparsedPublicKey<Bytes>,
 }
@@ -72,7 +77,7 @@ where
     }
 
     pub async fn try_respond(&self, req: Request<Body>) -> Result<Response<Body>, StatusCode> {
-        use hyper::{body, http::request::Parts, Method};
+        use hyper::{http::request::Parts, Method};
         let Self { rng, db, lobby, redirector, exchanger, http, public } = self;
         let (Parts { uri, method, headers, .. }, body) = req.into_parts();
         match (method, uri.path()) {
@@ -119,71 +124,8 @@ where
                 Ok(res)
             }
             (Method::GET, "/auth/callback") => {
-                let session = util::session::extract_session(&headers)?;
-                let oid = ObjectId::parse_str(session).map_err(|_| StatusCode::BAD_REQUEST)?;
-
-                // Check database if user ID is present
-                use model::session::Session;
-                let session = db
-                    .get_session(oid)
-                    .await
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-                    .ok_or(StatusCode::UNAUTHORIZED)?;
-                let nonce = if let Session::Pending { nonce } = session {
-                    nonce
-                } else {
-                    return Err(StatusCode::FORBIDDEN);
-                };
-
-                // Hash the salted session ID
-                use ring::digest;
-                let salted = util::session::salt_session_with_nonce(oid, nonce);
-                let hash = digest::digest(&digest::SHA256, &salted);
-
-                // Parse the `state` parameter as raw bytes
                 let query = uri.query().ok_or(StatusCode::BAD_REQUEST)?;
-                let (req, state) = exchanger.generate_token_request(query).ok_or(StatusCode::BAD_REQUEST)?;
-                let mut state_buf = [0; 32];
-                hex::decode_to_slice(state, &mut state_buf).map_err(|_| StatusCode::BAD_REQUEST)?;
-
-                // Validate whether the hash of the session matches
-                if hash.as_ref() != state_buf.as_ref() {
-                    return Err(StatusCode::BAD_REQUEST);
-                }
-
-                use body::Buf;
-                use model::oauth::TokenResponse;
-                let body = http.request(req).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.into_body();
-                let reader = body::aggregate(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.reader();
-                let TokenResponse { access, refresh, expires } =
-                    serde_json::from_reader(reader).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-                use twilight_model::user::CurrentUser;
-                let client = twilight_http::Client::new(access.clone().into_string());
-                let CurrentUser { id, .. } = client
-                    .current_user()
-                    .exec()
-                    .await
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-                    .model()
-                    .await
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-                let expires = core::time::Duration::from_secs(expires.get());
-                let success = db
-                    .upgrade_session(oid, id.into_nonzero(), access, refresh, expires)
-                    .await
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-                if !success {
-                    return Err(StatusCode::INTERNAL_SERVER_ERROR);
-                }
-
-                use hyper::header::HeaderValue;
-                let mut res = Response::new(Body::empty());
-                *res.status_mut() = StatusCode::FOUND;
-                assert!(res.headers_mut().insert("Location", HeaderValue::from_static("/")).is_none());
-                Ok(res)
+                callback::try_respond(&headers, query, exchanger, db, http).await
             }
             (Method::GET, _) => Err(StatusCode::NOT_FOUND),
             (_, "/discord" | "/quiz" | "/auth/login" | "/auth/callback") => Err(StatusCode::METHOD_NOT_ALLOWED),
