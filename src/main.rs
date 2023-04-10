13ccba77e17f4bc0debde7aa73d05b434ef17b7b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-10 17:05:26 +0800

    feat: add logs to main app

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn resolve_error_code(code: StatusCode) -> Response<Body> {
 }
 
 fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    log::info!("Starting up");
+
     // Retrieve the public key
     use std::env::{var, VarError};
     let key = var("PUB_KEY")?;
@@ -36,6 +39,9 @@ fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, app_port))?;
     listener.set_nonblocking(true)?;
 
+    let addr = listener.local_addr()?;
+    log::info!("Listening to {addr}");
+
     // Set up runtime
     let runtime = tokio::runtime::Builder::new_multi_thread().enable_io().enable_time().build()?;
     let tcp = {
@@ -47,6 +53,7 @@ fn main() -> anyhow::Result<()> {
         use core::pin::pin;
         let (client, connection) = config.connect(api::NoTls).await?;
         let mut postgres = pin!(runtime.spawn(connection));
+        log::info!("PostgreSQL driver connected");
 
         let app = api::App::new(client.into(), app_id, bot_token, pub_key);
         let state = std::sync::Arc::new(app);
@@ -70,16 +77,22 @@ fn main() -> anyhow::Result<()> {
                     continue;
                 }
                 stop_res = &mut stop => {
+                    log::info!("Stop signal received");
                     stop_res?;
                     break;
                 },
                 conn_res = &mut postgres => {
+                    log::info!("PostgreSQL disconnected");
                     conn_res??;
                     break;
                 },
                 else => continue,
             }
         }
+
         anyhow::Ok(())
-    })
+    })?;
+
+    log::info!("Shutting down");
+    Ok(())
 }
