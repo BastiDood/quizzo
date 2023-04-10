1bf22c7ea8c740746c1fe7e606b1378b1d9000d7
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-04-11 03:53:40 +0800

    chore: run `cargo fmt`

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -34,14 +34,14 @@ impl App {
                     log::error!("Unexpected `{method} {path}` request received");
                     return Err(StatusCode::NOT_FOUND);
                 }
-            }
+            },
             Method::POST => match path {
                 "/discord" => (),
                 _ => {
                     log::error!("Unexpected `POST {path}` request received");
                     return Err(StatusCode::NOT_FOUND);
                 }
-            }
+            },
             _ => {
                 log::error!("Unexpected `{method} {path}` request received");
                 return Err(StatusCode::METHOD_NOT_ALLOWED);
